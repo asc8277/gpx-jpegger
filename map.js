ee3062510b9648a23b5fcd99c216299253d266da
Andreas Schmidt <asc@e.mail.de>
2021-07-06 09:31:00 +0200

    refactor

--- map.js
+++ map.js
@@ -1,17 +1,18 @@
 import { promises as fs } from 'fs';
-import { resolve } from 'path';
-import { Builder } from 'selenium-webdriver';
-import { Options } from 'selenium-webdriver/chrome.js';
+import path from 'path';
+import Webdriver from 'selenium-webdriver';
+import Chrome from 'selenium-webdriver/chrome.js';
 import Jimp from 'jimp';
-import express, { static as staticMiddleware } from 'express';
+import express from 'express';
 import HttpTerminator from 'http-terminator';
 import getPort from 'get-port';
-
-const { read } = Jimp;
-const { createHttpTerminator } = HttpTerminator;
+import { fileURLToPath } from 'url';
 
 // chromium (or chrome) and chromedriver need to be installed
 // e.g. apt install chromium-browser chromium-chromedriver
+
+const localdir = path.dirname(fileURLToPath(import.meta.url));
+const tempfile = path.resolve(localdir, 'temp.gpx');
 
 (async () => {
   const dir = process.argv[2];
@@ -19,12 +20,22 @@ const { createHttpTerminator } = HttpTerminator;
 
   const port = await getPort();
   const url = `http://localhost:${port}/map.html`;
-  const app = express();
-  app.use(staticMiddleware('.'));
-  const server = app.listen(port);
-  const httpTerminator = createHttpTerminator({ server });
-
+  const httpTerminator = HttpTerminator.createHttpTerminator({
+    server: express().use(express.static(localdir)).listen(port)
+  });
   console.log(url);
+
+  const options = new Chrome.Options();
+  options.addArguments('--headless');
+  options.addArguments('--hide-scrollbars');
+  options.addArguments('--disable-gpu');
+  options.addArguments('--window-size=1920,1080');
+  options.addArguments('--force-device-scale-factor=2');
+
+  const driver = await new Webdriver.Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build();
 
   for (const file of files) {
     if (file.slice(-4) !== '.gpx') {
@@ -32,46 +43,22 @@ const { createHttpTerminator } = HttpTerminator;
     }
     console.log(file);
 
-    try {
-      await fs.unlink('temp.png');
-      await fs.unlink('temp.jpg');
-      await fs.unlink('temp.gpx');
-    } catch (e) {};
+    try { await fs.unlink(tempfile); } catch (e) { /* don't care */ }
 
-    await fs.copyFile(resolve(dir, file), 'temp.gpx');
-
-    const options = new Options();
-    options.addArguments('--headless');
-    options.addArguments('--hide-scrollbars');
-    options.addArguments('--disable-gpu');
-    options.addArguments('--window-size=1920,1080');
-    options.addArguments('--force-device-scale-factor=2');
-
-    const driver = await new Builder()
-      .forBrowser('chrome')
-      .setChromeOptions(options)
-      .build();
-
+    await fs.copyFile(path.resolve(dir, file), tempfile);
     await driver.get(url);
     await new Promise((resolve) => { setTimeout(resolve, 5000); });
-    const data = await driver.takeScreenshot();
-
-    await fs.writeFile('temp.png', data, 'base64');
-    console.log(`${file} png written`);
-
-    const png = await read('temp.png');
-    await png.quality(90).writeAsync('temp.jpg');
+    const screenshot = await driver.takeScreenshot();
+    const png = await Jimp.read(Buffer.from(screenshot, 'base64'));
+    await png.quality(90).writeAsync(`${path.resolve(dir, file).slice(0, -4)}.jpg`);
     console.log(`${file} jpg written`);
 
-    await fs.copyFile('temp.jpg', `${resolve(dir, file).slice(0, -4)}.jpg`);
-    console.log(`${file} jpg copied`);
-
-    fs.unlink('temp.png');
-    fs.unlink('temp.jpg');
-    fs.unlink('temp.gpx');
-    console.log(`${file} deleted temp files`);
-
-    await httpTerminator.terminate();
-    await driver.quit();
+    try { await fs.unlink(tempfile); } catch (e) { /* don't care */ }
   }
+
+  await Promise.all([
+    httpTerminator.terminate(),
+    driver.quit()
+  ]);
+  console.log('complete');
 })();
